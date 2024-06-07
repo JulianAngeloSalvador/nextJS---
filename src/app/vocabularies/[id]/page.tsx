e2aa13a4219610b8384be4889bf7d46b788aa5d9
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-06-07 19:05:12 +0800

    feat(/vocabs): implemented not-found page

--- src/app/vocabularies/[id]/page.tsx
+++ src/app/vocabularies/[id]/page.tsx
@@ -1,8 +1,13 @@
 import { capitalizeFirst } from "@/app/utils";
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true;
 
 // generateStaticParams statically generates routes at build time instead of on-demand req (Perf wise)
 export async function generateStaticParams() {
-  const res = await fetch(`${process.env.API_URL}`).then((res) => res.json());
+  const res = await fetch(`${process.env.API_URL}/vocabularies`).then((res) =>
+    res.json()
+  );
 
   return res.map((vocab: { id: string }) => ({
     id: vocab.id,
@@ -10,27 +15,27 @@ export async function generateStaticParams() {
 }
 
 const getVocab = async (id: string) => {
+  // await new Promise((resolve) => setTimeout(resolve, 5000)); // loader simulator
   const res = await fetch(`${process.env.API_URL}/vocabularies/${id}`);
+
+  // handling non-existen params using NextJS' not found handler
+  if (!res.ok) {
+    notFound();
+  }
 
   return res.json();
 };
 
 export default async function page({ params }: { params: { id: string } }) {
-  // handled non-existent params through try-catch block but can also use NextJS' dynamicParams
-  try {
-    const entryData: Vocabularies = await getVocab(params.id);
-    const { title, id, contentDescription } = entryData;
+  const entryData: Vocabularies = await getVocab(params.id);
+  const { title, id, contentDescription } = entryData;
 
-    return (
-      <section>
-        <article key={id}>
-          <h2>{title}</h2>
-          <p className="text-tertiary">{capitalizeFirst(contentDescription)}</p>
-        </article>
-      </section>
-    );
-  } catch (error) {
-    console.error("Error fetching vocabulary:", error);
-    return <p>Vocabulary not found.</p>;
-  }
+  return (
+    <section>
+      <article key={id}>
+        <h2>{title}</h2>
+        <p className="text-tertiary">{capitalizeFirst(contentDescription)}</p>
+      </article>
+    </section>
+  );
 }
