0f1156cff931e13d60317c9a44db42c58da12abf
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-06-07 10:13:29 +0800

    feat(/types): added type for Vocabularies

---
+++ src/app/types.ts
@@ -0,0 +1,46 @@
+type Vocabularies = {
+  id: string;
+  title: string;
+  kanji?: string;
+  romaji: string;
+  hiragana: string;
+  type: string;
+  translation?: string[];
+  conjugation?: [
+    {
+      positive: [
+        {
+          present: string;
+          kanji?: string;
+          hiragana: string;
+          politeness: "formal" | "informal";
+          alt?: string[];
+        },
+        {
+          past: string;
+          kanji?: string;
+          hiragana: string;
+          politeness: "formal" | "informal";
+          alt?: string[];
+        }
+      ];
+      negative: [
+        {
+          present: string;
+          kanji?: string;
+          hiragana: string;
+          politeness: "formal" | "informal";
+          alt?: string[];
+        },
+        {
+          past: string;
+          kanji?: string;
+          hiragana: string;
+          politeness: "formal" | "informal";
+          alt?: string[];
+        }
+      ];
+    }
+  ];
+  contentDescription: string;
+};
