99d3d37f85e17fe26e5142da6a0c0f55e85a3198
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-06-07 11:32:43 +0800

    style(mainnav): handled responsiveness

--- src/app/components/MainNav.tsx
+++ src/app/components/MainNav.tsx
@@ -2,36 +2,44 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import { Icon } from "@iconify-icon/react";
 
 const paths = [
   {
     title: "Home",
     path: "/",
+    icon: "tabler:home",
   },
   {
     title: "Vocabularies",
     path: "/vocabularies",
+    icon: "streamline:dictionary-language-book",
   },
   {
     title: "Learn",
     path: "/learn",
+    icon: "tabler:book-2",
   },
 ];
 export default function MainNav() {
   const pathname = usePathname();
   return (
-    <nav className="w-full mx-auto mt-8 relative after:absolute after:h-0.5 after:bg-tertiary after:w-full after:left-0 after:bottom-0 p-4 after:rounded-full">
-      <ul className="flex gap-x-8 items-center">
+    <nav className="w-full mt-8 fixed sm:relative bottom-0 sm:p-4 after:absolute after:h-0.5 after:bg-tertiary after:w-full after:left-0 after:top-0 sm:after:top-full after:rounded-full flex justify-center md:justify-start">
+      <ul className="flex sm:gap-x-8 flex-grow sm:flex-grow-0">
         {paths.map((link) => (
-          <li key={link.title}>
+          <li key={link.title} className="flex-grow">
             <Link
               href={link.path}
-              className={`font-poppins font-semibold hover:text-secondary px-3 py-1 transition-colors duration-fast ease-in-out ${
-                pathname === link.path ? "text-cta" : "text-tertiary"
+              className={`flex text-center font-poppins font-semibold hover:text-cta xl:hover:text-secondary transition-colors duration-fast ease-in-out py-3 px-2 sm:pb-0 ${
+                pathname === link.path
+                  ? "text-cta hover:text-cta"
+                  : "text-tertiary"
               }`}
             >
-              {link.title}
+              <span className="hidden sm:inline-block">{link.title}</span>
+              <span className="inline-block sm:hidden text-icon w-full text-center">
+                <Icon icon={link.icon} />
+              </span>
             </Link>
           </li>
         ))}
