952f532a03d5d97f9e34a5de435c848ce1f317cd
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-06-07 19:40:47 +0800

    feat(/vocab): imported nanum myeongjo font-fam

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -1,10 +1,10 @@
 import { Poppins, Nanum_Myeongjo, Montserrat } from "next/font/google";
 
-// export const nanumMyeongjo = Nanum_Myeongjo({
-//   weight: ["400", "700", "800"],
-//   variable: "--font-nanum-myeongjo",
-//   subsets: ["latin"],
-// });
+export const nanumMyeongjo = Nanum_Myeongjo({
+  weight: ["400", "700", "800"],
+  variable: "--font-nanum-myeongjo",
+  subsets: ["latin"],
+});
 
 export const poppins = Poppins({
   weight: ["100", "400", "600", "900"],
--- src/app/vocabularies/[id]/not-found.tsx
+++ src/app/vocabularies/[id]/not-found.tsx
@@ -1,11 +1,11 @@
-import { montserrat } from "@/app/fonts/fonts";
+import { nanumMyeongjo } from "@/app/fonts/fonts";
 import Link from "next/link";
 import React from "react";
 
 export default function NotFound() {
   return (
     <article className="flex flex-col gap-y-5 text-center w-fluid-500 mx-auto text-balance px-5 mt-8">
-      <h2 className={`${montserrat.className}`}>ヤバイ！</h2>
+      <h2 className={`${nanumMyeongjo.className}`}>ヤバイ！</h2>
       <p>We couldn't find the word you we're looking for 😥</p>
       <Link
         href={"/vocabularies"}
