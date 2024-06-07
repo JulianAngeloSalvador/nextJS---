521e739f87632c8c23d2f26ce72b268f8d8b216a
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-06-07 18:46:00 +0800

    style(tailwind.config): added fluid width for 600px & updated font-fam classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -34,12 +34,20 @@ const config: Config = {
         sm: "540px",
       },
       fontFamily: {
-        "zen-old": ['"var(--font-zen-old)"', ...defaultTheme.fontFamily.sans],
+        "nanum-myeongjo": [
+          '"var(--font-nanum-myeongjo)"',
+          ...defaultTheme.fontFamily.sans,
+        ],
         poppins: ['"var(--font-poppins)"', ...defaultTheme.fontFamily.sans],
+        montserrat: [
+          '"var(--font-montserrat)"',
+          ...defaultTheme.fontFamily.sans,
+        ],
       },
       width: {
         "fluid-150": "min(150px, 100%)",
         "fluid-200": "min(200px, 100%)",
+        "fluid-600": "min(600px, 100%)",
         "fluid-800": "min(800px, 100% - 1rem)",
         "fluid-1200": "min(1200px, 100% - 1rem)",
         "fluid-1400": "min(1400px, 100% - 1rem)",
