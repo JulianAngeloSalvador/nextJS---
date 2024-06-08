4b010179ea164c432ff11a261ef747f991ad41b4
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-06-08 10:43:40 +0800

    style(styles/global): handled input autofill styling and scroll bar

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { nanumMyeongjo } from "../fonts/fonts";
 
 export default function AuthLayout({
   children,
@@ -9,7 +10,7 @@ export default function AuthLayout({
   return (
     <section className="w-full h-dvh bg-primary flex flex-col justify-center items-center">
       <main className=" w-fluid-400 z-10">
-        <h1 className="text-center text-nowrap">
+        <h1 className={`${nanumMyeongjo.className} text-center text-nowrap`}>
           {pathname === "/auth/login" && "おかえり"}
           {pathname === "/auth/register" && "ようこそ"}
         </h1>
