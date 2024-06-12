06b65f1f9e53546921dcbf84cd875beaceb63137
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-06-12 17:51:15 +0800

    feat(auth/actions): added first & last name options in user_metadata

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -28,9 +28,14 @@ export async function signup(formData: FormData) {
   const supabase = createClient();
 
   const data = {
-    name: formData.get("name") as string,
     email: formData.get("email") as string,
     password: formData.get("password") as string,
+    options: {
+      data: {
+        first_name: formData.get("first_name") as string,
+        last_name: formData.get("last_name") as string,
+      },
+    },
   };
 
   const { error } = await supabase.auth.signUp(data);
